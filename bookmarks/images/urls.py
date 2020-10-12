8373bb3ff93add24f27b50932b64c9241ec82256
Alexandre Guillaume Nicolas CAMBEFORT <acambefort@pentalog.com>
2020-10-12 17:54:02 +0300

    added infinite scroll

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('detail/<id>/<slug>/', views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like'),
+    path('', views.image_list, name='list'),
 ]
