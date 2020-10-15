35c3b0c03e690a986d7d6042004d40308d0887ed
Alexandre Guillaume Nicolas CAMBEFORT <acambefort@pentalog.com>
2020-10-15 15:10:08 +0300

    added jwt

--- bookmarks/api_example/urls.py
+++ bookmarks/api_example/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import (
 from . import views
 from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_simplejwt import views as jwt_views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -18,6 +19,8 @@ urlpatterns = [
     path('images/<slug>', views.ImageList.as_view()),
     path('images/<int:pk>/', views.ImageDetail.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
